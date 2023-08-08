944f34491a47a81f6d2b384dadad3cbfb6a04323
Piotr Bogacz <piotr.bogacz7@wp.pl>
2023-08-08 17:46:29 +0200

    Get quotes from API, set quote and author  on website, show and remove loading spinner

---
+++ script.js
@@ -0,0 +1,43 @@
+// Variables
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const quoteAuthor = document.getElementById("author");
+const loader = document.getElementById("loader");
+
+// Quotes array
+let apiQuotes = [];
+
+function showLoadingSpinner() {
+    quoteContainer.hidden = true;
+    loader.hidden = false;
+}
+
+function removeLoadingSpinner() {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+}
+
+// Choose next quote 
+function newQuote() {
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    quoteText.textContent = quote.text;
+    quoteAuthor.textContent = quote.author;
+}
+
+// Get quotes from API
+async function getQuotes() {
+    showLoadingSpinner();
+    // Set API url
+    const apiUrl = "https://jacintodesign.github.io/quotes-api/data/quotes.json";
+    try {
+        const response = await fetch(apiUrl);
+        apiQuotes = await response.json();
+        newQuote();
+    } catch (error) {
+        console.log(error);
+    }
+    removeLoadingSpinner();
+}
+
+// On load
+getQuotes();
